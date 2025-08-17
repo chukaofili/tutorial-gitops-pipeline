1c9a3729589296603331ab598a69cb5268ec4465
Chuka Ofili <chuka.ofili.us@gmail.com>
2025-08-17 17:07:16 -0500

    chore: update default GKE machine type from e2-standard-8 to n2-standard-4

--- part-1/terraform/infrastructure/variables.tf
+++ part-1/terraform/infrastructure/variables.tf
@@ -52,7 +52,7 @@ variable "gke_node_disk_type" {
 variable "gke_machine_type" {
   description = "Machine type for GKE nodes"
   type        = string
-  default     = "e2-standard-8"
+  default     = "n2-standard-4"
 }
 
 # Cloud SQL Variables
