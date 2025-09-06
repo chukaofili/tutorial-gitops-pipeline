210314f62aeee462e68418771cef179d5c6dbc11
Chuka Ofili <chuka.ofili.us@gmail.com>
2025-09-06 22:31:25 +0100

    chore: remove deprecated GitHub organization and token variables from Terraform configuration

--- part-1/terraform/init/tfc_tfe_variables.tf
+++ part-1/terraform/init/tfc_tfe_variables.tf
@@ -86,20 +86,3 @@ resource "tfe_variable" "terraform_cloud_workspace_name" {
   category     = "terraform"
   description  = "Terraform Cloud workspace"
 }
-
-resource "tfe_variable" "terraform_cloud_github_organization_name" {
-  workspace_id = tfe_workspace.main_workspace.id
-  key          = "github_organization"
-  value        = var.github_organization
-  category     = "terraform"
-  description  = "GitHub organization"
-}
-
-resource "tfe_variable" "terraform_cloud_github_token" {
-  workspace_id = tfe_workspace.main_workspace.id
-  key          = "github_token"
-  value        = var.github_token
-  category     = "terraform"
-  sensitive    = true
-  description  = "GitHub personal access token with admin permissions"
-}
