510787d4f9ddd15ba1edf76893e6585200dfb0cd
Chuka Ofili <chuka.ofili.us@gmail.com>
2025-09-06 21:47:12 +0100

    chore: add NAT router configuration to Terraform infrastructure

---
+++ part-2/add.infrstructure.nat-router.tf
@@ -0,0 +1,19 @@
+# Copy this file part-1/terraform/infrastructure/nat-router.tf
+
+data "google_compute_network" "default" {
+  name = "default"
+}
+
+resource "google_compute_router" "nat-router" {
+  name    = "nat-router"
+  network = data.google_compute_network.default.id
+  region  = var.google_region
+}
+
+resource "google_compute_router_nat" "main" {
+  name                               = "cloud-nat"
+  region                             = google_compute_router.nat-router.region
+  router                             = google_compute_router.nat-router.name
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
