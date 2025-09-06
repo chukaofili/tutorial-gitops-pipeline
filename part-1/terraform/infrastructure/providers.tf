d6d771de10caa33ac66c9bfc9ea291f60e79c482
Chuka Ofili <chuka.ofili.us@gmail.com>
2025-09-06 22:28:04 +0100

    chore: remove unused provider configurations and variables for Flux and GitHub

--- part-1/terraform/infrastructure/providers.tf
+++ part-1/terraform/infrastructure/providers.tf
@@ -8,22 +8,6 @@ terraform {
       source  = "hashicorp/random"
       version = "3.7.2"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.38.0"
-    }
-    github = {
-      source  = "integrations/github"
-      version = "6.6.0"
-    }
-    flux = {
-      source  = "fluxcd/flux"
-      version = "1.6.4"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "4.1.0"
-    }
   }
 
   required_version = "~> 1.12"
@@ -32,8 +16,4 @@ terraform {
 provider "google" {
   project = var.google_project_id
   region  = var.google_region
-}
-
-provider "github" {
-  owner = var.github_organization
 }
--- part-1/terraform/infrastructure/variables.tf
+++ part-1/terraform/infrastructure/variables.tf
@@ -86,33 +86,3 @@ variable "sql_user_name" {
   default     = "app_user"
 }
 
-// New variables added From Part 2
-
-variable "github_organization" {
-  description = "Github organization name, use this to specify the github organization name to use for terraform cloud this can be either github individual or a team org account"
-  type        = string
-}
-
-variable "flux_cluster_name" {
-  description = "FluxCD cluster name"
-  type        = string
-  default     = "primary-cluster"
-}
-
-variable "flux_repository" {
-  description = "FluxCD Github repository name, use this to specify the github repository used for fluxcd to manage the cluster state"
-  type        = string
-  default     = "tutorial-gitops-fluxcd" # Change this to wwhatever you named your repository in part-1 -> provision the infra -> step 6
-}
-
-variable "flux_repository_branch" {
-  description = "FluxCD git repository branch to use"
-  type        = string
-  default     = "main"
-}
-
-variable "github_token" {
-  description = "GitHub personal access token for API access"
-  type        = string
-  sensitive   = true
-}
