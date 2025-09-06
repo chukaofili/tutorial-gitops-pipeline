1b30c05408f4da0f4e7cfa75cf0b6c85f6114aa2
Chuka Ofili <chuka.ofili.us@gmail.com>
2025-09-06 21:50:12 +0100

    chore: enhance NAT router configuration with detailed comments and structure

--- part-1/terraform/infrastructure/nat-router.tf
+++ part-1/terraform/infrastructure/nat-router.tf
@@ -1,17 +1,49 @@
+# Copy this file part-1/terraform/infrastructure/nat-router.tf
+
+############################################
+# 1) Look up the default VPC network
+############################################
+# This data source fetches the "default" VPC network in your project.
+# We attach our Cloud Router and NAT to this network.
 data "google_compute_network" "default" {
   name = "default"
 }
 
+############################################
+# 2) Create a Cloud Router
+############################################
+# A Cloud Router is required for Cloud NAT.
+# It manages dynamic routes and acts as the control plane for NAT.
 resource "google_compute_router" "nat-router" {
-  name    = "nat-router"
+  # Name for the router resource
+  name = "nat-router"
+
+  # Attach the router to the default VPC
   network = data.google_compute_network.default.id
-  region  = var.google_region
+
+  # Region where the router (and NAT) will live.
+  # Should match the region of your GKE nodes.
+  region = var.google_region
 }
 
+############################################
+# 3) Configure Cloud NAT
+############################################
+# Cloud NAT lets private resources (like GKE nodes without public IPs)
+# access the internet securely for pulling container images, updates, etc.
 resource "google_compute_router_nat" "main" {
-  name                               = "cloud-nat"
-  region                             = google_compute_router.nat-router.region
-  router                             = google_compute_router.nat-router.name
-  nat_ip_allocate_option             = "AUTO_ONLY"
+  # Name for the NAT configuration
+  name = "cloud-nat"
+
+  # Region and router this NAT belongs to
+  region = google_compute_router.nat-router.region
+  router = google_compute_router.nat-router.name
+
+  # AUTO_ONLY means Google will assign ephemeral external IPs for NAT.
+  # You can switch to MANUAL if you want to reserve and manage static IPs.
+  nat_ip_allocate_option = "AUTO_ONLY"
+
+  # Apply NAT to all subnets and all IP ranges in this region.
+  # This ensures every private GKE node in the VPC can reach the internet.
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 }
