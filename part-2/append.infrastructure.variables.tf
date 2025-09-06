279b455c9356f332c6ee15fccc954bb85a34be55
Chuka Ofili <chuka.ofili.us@gmail.com>
2025-09-06 20:15:26 +0100

    chore: add GitHub organization and repository variables for Terraform configuration

--- part-2/append.infrastructure.variables.tf
+++ part-2/append.infrastructure.variables.tf
@@ -1,3 +1,13 @@
+variable "github_organisation" {
+  description = "Github organisation name, use this to specify the github organisation name to use for terraform cloud this can be either github individual or a team org account"
+  type        = string
+}
+
+variable "github_repository" {
+  description = "Github repository name, use this to specify the github repository name to use for terraform cloud"
+  type        = string
+}
+
 variable "flux_cluster_name" {
   description = "Flux cluster config name"
   type        = string
